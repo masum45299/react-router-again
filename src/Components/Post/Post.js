5d42a915747364e052e63078a6365415e4dcb800
masum45299 <masum459299@gmail.com>
2023-02-16 23:58:29 +0600

    2nd push

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate} from 'react-router-dom';
 import './Post.css'
 
 const Post = ({post}) => {
     const {id,title}=post
+    const navigate=useNavigate()
+    const evenHandler= ()=>{
+        navigate(`/post/${id}`)
+    }
     return (
         <div className='post'>
             <p>Title : {title}</p>
             <button><Link to={`/post/${id}`}>Post</Link></button>
+            <button onClick={evenHandler}>Post2</button>
+
         </div>
     );
 };
 
-export default Post;
+export default Post; 
